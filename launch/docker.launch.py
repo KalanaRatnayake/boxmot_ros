1610068ce9409e9fad91a12c3b93a22ab7d93c11
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2024-07-22 12:52:26 +1000

    minor fix to launch file

--- launch/docker.launch.py
+++ launch/docker.launch.py
@@ -13,21 +13,21 @@ def generate_launch_description():
     yolo_yolo_model_param                = DeclareLaunchArgument('yolo_model',                      default_value="yolov9s.pt")
     yolo_input_rgb_topic_param           = DeclareLaunchArgument('input_rgb_topic',                 default_value="/camera/color/image_raw")
     yolo_input_depth_topic_param         = DeclareLaunchArgument('input_depth_topic',               default_value="/camera/depth/points")
-    yolo_subscribe_depth_param           = DeclareLaunchArgument('subscribe_depth',                 default_value=True)
-    yolo_publish_annotated_image_param   = DeclareLaunchArgument('yolo_publish_annotated_image',    default_value=True)
+    yolo_subscribe_depth_param           = DeclareLaunchArgument('subscribe_depth',                 default_value="True")
+    yolo_publish_annotated_image_param   = DeclareLaunchArgument('yolo_publish_annotated_image',    default_value="True")
     yolo_annotated_topic_param           = DeclareLaunchArgument('yolo_annotated_topic',            default_value="/yolo_ros/annotated_image")
     yolo_detailed_topic_param            = DeclareLaunchArgument('yolo_detailed_topic',             default_value="/yolo_ros/detection_result")
-    yolo_threshold_param                 = DeclareLaunchArgument('yolo_threshold',                  default_value=0.25)
-    yolo_device_param                    = DeclareLaunchArgument('yolo_device',                     default_value="0")
+    yolo_threshold_param                 = DeclareLaunchArgument('yolo_threshold',                  default_value="0.25")
+    yolo_device_param                    = DeclareLaunchArgument('yolo_device',                     default_value="'0'")
 
     boxmot_yolo_model_param              = DeclareLaunchArgument('tracking_model',                  default_value="deepocsort")
     boxmot_input_rgb_topic_param         = DeclareLaunchArgument('reid_model',                      default_value="osnet_x0_25_msmt17.pt")
     boxmot_input_depth_topic_param       = DeclareLaunchArgument('input_topic',                     default_value="/yolo_ros/detection_result")
-    boxmot_publish_annotated_image_param = DeclareLaunchArgument('boxmot_publish_annotated_image',  default_value=True)
+    boxmot_publish_annotated_image_param = DeclareLaunchArgument('boxmot_publish_annotated_image',  default_value="True")
     boxmot_annotated_topic_param         = DeclareLaunchArgument('boxmot_annotated_topic',          default_value="/boxmot_ros/annotated_image")
     boxmot_detailed_topic_param          = DeclareLaunchArgument('boxmot_detailed_topic',           default_value="/boxmot_ros/tracking_result")
-    boxmot_threshold_param               = DeclareLaunchArgument('boxmot_threshold',                default_value=0.25)
-    boxmot_device_param                  = DeclareLaunchArgument('boxmot_device',                   default_value="0")
+    boxmot_threshold_param               = DeclareLaunchArgument('boxmot_threshold',                default_value="0.25")
+    boxmot_device_param                  = DeclareLaunchArgument('boxmot_device',                   default_value="'0'")
 
     yolo_ros_node = launch_ros.actions.Node(package='yolo_ros',
                                               executable='yolo_ros',
